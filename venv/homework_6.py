07c4ba02f63c2ba2bc8b813a80d2e8ea4ae3dedb
andrey414 <andrii.kurochkin@nure.ua>
2023-08-07 10:28:29 +0300

    My homework_6.py

---
+++ venv/homework_6.py
@@ -0,0 +1,11 @@
+def fibonacci_recursive(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+n = int(input ("Input n: "))
+result = fibonacci_recursive(n)
+print(f"The {n}-th Fibonacci number is {result}")
